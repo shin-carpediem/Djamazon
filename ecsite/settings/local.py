b572bc62a358f5aaa6b06a8e506d61c85c623569
shin-carpediem <buru.aoshin@gmail.com>
2020-12-12 23:08:53 +0900

    hoge

--- ecsite/settings/local.py
+++ ecsite/settings/local.py
@@ -21,7 +21,7 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'static'),
+   os.path.join(BASE_DIR, 'app/static'),
 )
 
 # #誤ってHTTPによってCSRFクッキーを送信してしまうのを防ぐにはTrueをセットしてください。
