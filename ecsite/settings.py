052207a51e1d142805c23aa9a4b8caed9e412c90
shin-carpediem <buru.aoshin@gmail.com>
2020-12-28 23:46:27 +0900

    fix database

--- ecsite/settings.py
+++ ecsite/settings.py
@@ -30,7 +30,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -46,11 +45,20 @@ INSTALLED_APPS = [
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shinac$default',
+        'USER': 'shinac',
+        'PASSWORD': 'MY9@Eexhwe7GJkm',
     }
 }
 
