a858927647f2f5efd5f5b956a9fae6bfbde76217
shin-carpediem <buru.aoshin@gmail.com>
2020-12-29 11:28:23 +0900

    fix database

--- ecsite/wsgi.py
+++ ecsite/wsgi.py
@@ -8,13 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-# import sys
-# import pymysql
+import sys
+import pymysql
 
-# from django.core.wsgi import get_wsgi_application
-# from django.core.management import execute_from_command_line
+from django.core.wsgi import get_wsgi_application
+from django.core.management import execute_from_command_line
 
-# pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecsite.settings')
 
