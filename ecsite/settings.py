93a2d7a3cb4ce3a95e2c07cc3461b1b9cabf5329
shin-carpediem <buru.aoshin@gmail.com>
2021-01-02 22:17:13 +0900

    fix layout

--- ecsite/settings.py
+++ ecsite/settings.py
@@ -14,7 +14,7 @@ import os
 from django.utils.translation import ugettext_lazy as _
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
