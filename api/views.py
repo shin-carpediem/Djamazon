d9ced2f79ad133d4f6b9b1ea3a451b643dbde380
shin-carpediem <buru.aoshin@gmail.com>
2021-01-04 23:43:57 +0900

    in progress of api rest framework

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from users.models import User
 from app.models import Product, Sale
 from .serializers import UserSerializer, ProductSerializer, SaleSerializer
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 # Create your views here.
 # users
 
@@ -10,14 +11,17 @@ from rest_framework.generics import ListAPIView
 class UserListView(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    pagination_class = PageNumberPagination
 
 
 # app
 class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
 
 
 class SaleListView(ListAPIView):
     queryset = Sale.objects.all()
     serializer_class = SaleSerializer
+    pagination_class = PageNumberPagination
