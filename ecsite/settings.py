e3b1e5376e390573a243df9878bc66aa2d6ceec9
shin-carpediem <buru.aoshin@gmail.com>
2021-01-13 00:14:28 +0900

    opt anywhere

--- ecsite/settings.py
+++ ecsite/settings.py
@@ -94,8 +94,7 @@ MIDDLEWARE = [
 
 if DEBUG:
     ALLOWED_HOSTS = ['127.0.0.1']
-else:
-    ALLOWED_HOSTS = os.getenv("DEBUG_FALSE_ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.getenv("DEBUG_FALSE_ALLOWED_HOSTS")
 
 # The Debug Toolbar is shown
 # only if your IP address is listed in the INTERNAL_IPS setting.
