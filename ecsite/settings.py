27f836750bcd96b9a63a07109186f5a6e711854d
shin-carpediem <buru.aoshin@gmail.com>
2021-01-18 12:48:24 +0900

    fix setting

--- ecsite/settings.py
+++ ecsite/settings.py
@@ -15,6 +15,7 @@ import logging
 from dotenv import load_dotenv
 from django.utils.translation import ugettext_lazy as _
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
@@ -95,8 +96,8 @@ MIDDLEWARE = [
 if DEBUG:
     ALLOWED_HOSTS = ['127.0.0.1']
 else:
-    # ALLOWED_HOSTS = os.getenv("DEBUG_FALSE_ALLOWED_HOSTS")
-    ALLOWED_HOSTS = ['shinac.pythonanywhere.com']
+    ALLOWED_HOSTS = os.getenv("DEBUG_FALSE_ALLOWED_HOSTS")
+    # ALLOWED_HOSTS = ['shinac.pythonanywhere.com']
 
 # The Debug Toolbar is shown
 # only if your IP address is listed in the INTERNAL_IPS setting.
