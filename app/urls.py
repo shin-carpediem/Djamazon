93a42e193df2a7fa72b0e6401043c71a093b95f1
shin-carpediem <buru.aoshin@gmail.com>
2021-01-31 15:24:21 +0900

    create pw reset

--- app/urls.py
+++ app/urls.py
@@ -13,23 +13,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset/', views.password_reset, name='password_reset'),
-    path('admin/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
-    path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-#     path('reset/', auth_views.PasswordResetView.as_view(
-#         email_template_name='password_reset_email.html',
-#         subject_template_name='password_reset_subject.txt',
-#         name='password_reset'),),
-#     path('reset/done/', auth_views.PasswordResetDoneView.as_view(
-#         template_name='app/password_reset_done.html'),
-#         name='password_reset_done'),
-#     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-#         template_name='app/password_reset_confirm.html'),
-#         name='password_reset_confirm'),
-#     path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(
-#         template_name='app/password_reset_complete.html'),
-#         name='password_reset_complete'),
     path('product/<int:product_id>/', views.detail, name='detail'),
     path('fav_products/', views.fav_products, name='fav_products'),
     path('toggle_fav_product_status/', views.toggle_fav_product_status,
