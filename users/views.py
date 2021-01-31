044a1fafd792c56da5dde0c0cc3fd25a1581bc81
shin-carpediem <buru.aoshin@gmail.com>
2021-01-31 14:11:16 +0900

    create pw reset

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-import logging
+# import logging
 
 # Create your views here.
-logger = logging.getLogger(__name__)
-logger.info("log info test!")
-logger.error("log error test!")
+# logger = logging.getLogger(__name__)
+# logger.info("log info test!")
+# logger.error("log error test!")
 
 
 def owner_profile(request):
