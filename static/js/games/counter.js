570104a5dc1199139ff215d1c139d044b698921c
shin-carpediem <buru.aoshin@gmail.com>
2021-02-01 23:04:32 +0900

    make counter

---
+++ static/js/games/counter.js
@@ -0,0 +1,24 @@
+const counter = () => {
+  "use strict";
+
+  const num = 5;
+  const winner = Math.floor(Math.random() * num); // 0 ~ 4
+
+  for (let i = 0; i < num; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box");
+
+    div.addEventListener("click", () => {
+      if (i === winner) {
+        div.textContect = "Win!";
+        div.classList.add("win");
+      } else {
+        div.textContect = "Lose!";
+        div.classList.add("lose");
+      }
+    });
+
+    document.section.appendChild(div);
+  }
+};
+counter();
