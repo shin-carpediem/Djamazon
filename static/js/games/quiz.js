e5d89c6f5254d46428530545475bc46217dc2a71
shin-carpediem <buru.aoshin@gmail.com>
2021-02-05 00:50:58 +0900

    in progress/ make quiz

--- static/js/games/quiz.js
+++ static/js/games/quiz.js
@@ -1,1 +1,16 @@
 "use strict";
+
+{
+  const question = document.getElementById("quiz-question");
+  const choices = document.getElementById("quiz-choices");
+  const question = document.getElementById("quiz-btn");
+
+  const quizSet = [
+    { q: "What is A?", c: ["A0", "A1", "A2"] },
+    { q: "What is B?", c: ["B0", "B1", "B2"] },
+    { q: "What is C?", c: ["C0", "C1", "C2"] },
+  ];
+
+  // 今何問目の問いか変数で管理する
+  let currentNum = 0;
+}
