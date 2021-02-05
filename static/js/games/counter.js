b388c527e8e5c3a947e6f86d5c740ec4637ada6f
shin-carpediem <buru.aoshin@gmail.com>
2021-02-05 21:47:09 +0900

    fix couter.js

--- static/js/games/counter.js
+++ static/js/games/counter.js
@@ -1,24 +1,23 @@
 "use strict";
 
-const counter = () => {
+{
   const num = 5;
-  const winner = Math.floor(Math.random() * num); // 0 ~ 4
+  const winner = Math.floor(Math.random() * num); // 0 - 4
 
   for (let i = 0; i < num; i++) {
     const div = document.createElement("div");
     div.classList.add("box");
+
+    div.addEventListener("click", () => {
+      if (i === winner) {
+        div.textContent = "Win!";
+        div.classList.add("win");
+      } else {
+        div.textContent = "Lose!";
+        div.classList.add("lose");
+      }
+    });
+
+    document.getElementById("counterContent").appendChild(div);
   }
-
-  div.addEventListener("click", () => {
-    if (i === winner) {
-      div.textContect = "Win!";
-      div.classList.add("win");
-    } else {
-      div.textContect = "Lose!";
-      div.classList.add("lose");
-    }
-  });
-
-  document.getElementById("counterContent").appendChild(div);
-};
-counter();
+}
