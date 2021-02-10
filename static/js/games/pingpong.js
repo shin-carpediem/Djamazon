8daafe6dd9c00659891c9297eb158cd6ce61cf17
shin-carpediem <buru.aoshin@gmail.com>
2021-02-10 20:42:20 +0900

    create pingpong game

--- static/js/games/pingpong.js
+++ static/js/games/pingpong.js
@@ -1,4 +1,207 @@
 "use strict";
 
-{
-}
+// returnは関数にしか使えないので、以下を即時関数にする
+(() => {
+  // ボールが生成される位置や速度をランダムにする。Ballクラスや他のクラスとも関係ないので、ユーティリティとして一番上に置いとく
+  function rand(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  class Ball {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.ctx = this.canvas.getContext("2d");
+      this.x = rand(30, 250);
+      this.y = 30;
+      this.r = 10;
+      this.vx = rand(3, 5) * (Math.random() < 0.5 ? 1 : -1); //0.5より小さかったら1、そうではなかったら-1の50%にする
+      this.vy = rand(3, 5);
+      this.isMissed = false;
+    }
+
+    getMissedStatus() {
+      return this.isMissed;
+    }
+
+    bounce() {
+      this.vy *= -1;
+    }
+
+    reposition(paddleTop) {
+      this.y = paddleTop - this.r;
+    }
+
+    getX() {
+      return this.x;
+    }
+
+    getY() {
+      return this.y;
+    }
+
+    getR() {
+      return this.r;
+    }
+
+    update() {
+      this.x += this.vx;
+      this.y += this.vy;
+
+      if (this.y - this.r > this.canvas.height) {
+        this.isMissed = true;
+      }
+
+      // ボールを跳ね返らせる処理
+      if (
+        // 左の壁に当たった場合（差分の半径分を引かないと、半径分壁にめり込んでから軌道が反転(*=-1)になる。）
+        this.x - this.r < 0 ||
+        // 右の壁に当たった場合
+        this.x + this.r > this.canvas.width
+      ) {
+        this.vx *= -1;
+      }
+
+      if (this.y - this.r < 0) {
+        this.vy *= -1;
+      }
+    }
+
+    draw() {
+      this.ctx.beginPath();
+      this.ctx.fillStyle = "#fdfdfd";
+      // arcメソッドで円を描画
+      this.ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+      this.ctx.fill();
+    }
+  }
+
+  class Paddle {
+    constructor(canvas, game) {
+      this.canvas = canvas;
+      this.game = game;
+      this.ctx = this.canvas.getContext("2d");
+      this.w = 60;
+      this.h = 16;
+      this.x = this.canvas.width / 2 - this.w / 2;
+      this.y = this.canvas.height - 32;
+      this.mouseX = this.x;
+      this.addHandler();
+    }
+
+    addHandler() {
+      document.addEventListener("mousemove", (e) => {
+        this.mouseX = e.clientX;
+      });
+    }
+
+    update(ball) {
+      const ballBottom = ball.getY() + ball.getR();
+      const paddleTop = this.y;
+      const ballTop = ball.getY() - ball.getR();
+      const paddleBottom = this.y + this.h;
+      const ballCenter = ball.getX();
+      const paddleLeft = this.x;
+      const paddleRight = this.x + this.w;
+
+      if (
+        ballBottom > paddleTop &&
+        ballTop < paddleBottom &&
+        ballCenter > paddleLeft &&
+        ballCenter < paddleRight
+      ) {
+        ball.bounce();
+        // ボールが浅く入ってくるとパドルの中で跳ね返ってしまい、それを防ぐ
+        ball.reposition(paddleTop);
+        this.game.addScore();
+      }
+
+      const rect = this.canvas.getBoundingClientRect(); //canvasの位置を取得するメソッド
+      this.x = this.mouseX - rect.left - this.w / 2;
+
+      // canvasの左右端よりもパドルが行ってしまわないようにする
+      if (this.x < 0) {
+        this.x = 0;
+      }
+      if (this.x + this.w > this.canvas.width) {
+        this.x = this.canvas.width - this.w;
+      }
+    }
+
+    draw() {
+      this.ctx.fillStyle = "#fdfdfd";
+      this.ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+  }
+
+  class PingPongGame {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.ctx = this.canvas.getContext("2d");
+      this.ball = new Ball(this.canvas);
+      this.paddle = new Paddle(this.canvas, this);
+      this.loop();
+      this.isGameOver = false;
+      this.score = 0;
+    }
+
+    addScore() {
+      this.score++;
+    }
+
+    // "ゲームループ"と呼ばれる
+    loop() {
+      if (this.isGameOver) {
+        return;
+      }
+
+      this.update();
+      this.draw();
+
+      // 再帰的に呼び出すには setTimeout() を使ってもいいのですが、描画処理に最適化された requestAnimationFrame() というメソッドを使えばブラウザの次の描画タイミングに合わせて渡した関数を実行してくれます。
+      requestAnimationFrame(() => {
+        this.loop();
+      });
+    }
+
+    update() {
+      this.ball.update();
+      this.paddle.update(this.ball);
+
+      if (this.ball.getMissedStatus()) {
+        this.isGameOver = true;
+      }
+    }
+
+    draw() {
+      if (this.isGameOver) {
+        this.drawGameOver();
+        // ゲームオーバーになったらそれ以降の処理はしなくていいので、returnを返す
+        return;
+      }
+      // ボールが動いた後の軌跡を消す為に、clearRectをここで指定する
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ball.draw();
+      this.paddle.draw();
+      this.drawScore();
+    }
+
+    drawGameOver() {
+      this.ctx.font = '28px "Arial Black"';
+      this.ctx.fillStyle = "tomato";
+      this.ctx.fillText("GAME OVER", 50, 150);
+    }
+
+    drawScore() {
+      this.ctx.font = "20px Arial";
+      this.ctx.fillStyle = "#fdfdfd";
+      this.ctx.fillText(this.score, 10, 25);
+    }
+  }
+
+  const canvas = document.querySelector("canvas");
+  if (typeof canvas.getContext === "undefined") {
+    return;
+  }
+
+  new PingPongGame(canvas);
+})();
