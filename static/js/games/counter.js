417ce125eb51dddcf29a521115f2e5b0a6d337a4
shin-carpediem <buru.aoshin@gmail.com>
2021-02-12 16:40:25 +0900

    hoge

--- static/js/games/counter.js
+++ static/js/games/counter.js
@@ -1,76 +1,80 @@
 "use strict";
 
 {
-  // ボックスの個数の初期値
+  // ⓪：ボックスの個数の初期値
   let num = 3;
+  let winner = Math.floor(Math.random() * num); // num = 3 の時は、0 - 2
 
   // https://code-kitchen.dev/html/input-range/
-  // input要素を取得
+  // ⓪：要素を取得
+  const counterContent = document.getElementById("counterContent");
   const changelevelElem = document.getElementById("counterChangeLevel");
-  const changelevelValueElem = changelevelElem.value;
   const currentValueElem = document.getElementById("counter-current-value");
 
-  // ①numの値を決める関数
-  const changeLevel = (lev) => {
-    console.log(lev);
-    if (lev == 1) {
-      num = 3;
-      console.log(num);
-      return num;
-    } else if (lev == 2) {
-      num = 5;
-      console.log(num);
-      return num;
-    } else if (lev == 3) {
-      num = 10;
-      console.log(num);
-      return num;
-    } else if (lev == 4) {
-      num = 15;
-      console.log(num);
-      return num;
-    } else {
-      num = 15;
-      console.log(num);
-      return num;
-    }
-  };
+  // ①：ゲームをセッティングする関数
+  function setGame(num) {
+    for (let i = 0; i < num; i++) {
+      const div = document.createElement("div");
+      div.classList.add("box");
 
-  // ②現在の値をspanに埋め込む関数
+      div.addEventListener("click", () => {
+        if (i === winner) {
+          div.textContent = "Win!";
+          div.classList.add("win");
+        } else {
+          div.textContent = "Lose!";
+          div.classList.add("lose");
+        }
+      });
+
+      counterContent.appendChild(div);
+    }
+  }
+
+  // ゲームレベルの初期値をボックス3個として設置しておく
+  setGame(3);
+
+  // ②：現在のレベルをレベル表示箇所に埋め込む関数
   function setCurrentValue(val) {
     currentValueElem.innerText = val;
   }
 
-  // ①②をターゲットのvalueに応じて実行する関数
-  const rangeOnChange = (e) => {
-    const newlevel = e.target.value;
-    changeLevel(newlevel);
-    setCurrentValue(newlevel);
-  };
+  // ③：①のゲームレベルを変える関数
+  function changeLevel(lev) {　// ここで引数としてターゲットvalueを受け取れていない
+    switch (lev) {
+      case 1:
+        print(lev)
+        setGame(3);
+        break;
+      case 2:
+        setGame(4);
+        break;
+      case 3:
+        setGame(5);
+        break;
+      case 4:
+        setGame(7);
+        break;
+      case 5:
+        setGame(10);
+        break;
+    }
+  }
 
-  // スライダー変化時に②を発火
+  // ④：②③をターゲットのvalueの値に応じて実行
+  function rangeOnChange(e) {
+    setCurrentValue(e.target.value); //②
+    changeLevel(e.target.value); //③
+  }
+
+  // ⑤：②をウィンドウロード時に発火
   window.onload = () => {
     changelevelElem.addEventListener("input", rangeOnChange);
-    setCurrentValue(changelevelValueElem);
+    setCurrentValue(changelevelElem.value);
+    changeLevel(changelevelElem.value);
   };
 
-  // 以下はゲームロジック
-  let winner = Math.floor(Math.random() * num); // num = 3 の時は、0 - 2
-
-  for (let i = 0; i < num; i++) {
-    const div = document.createElement("div");
-    div.classList.add("box");
-
-    div.addEventListener("click", () => {
-      if (i === winner) {
-        div.textContent = "Win!";
-        div.classList.add("win");
-      } else {
-        div.textContent = "Lose!";
-        div.classList.add("lose");
-      }
-    });
-
-    document.getElementById("counterContent").appendChild(div);
-  }
+  // ⑥：③をrangeのvalueの値毎に発火
+  // changelevelElem.addEventListener("input", rangeOnChange);
+  // changeLevel(changelevelElem.value);
 }
