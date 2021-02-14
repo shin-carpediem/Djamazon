77d9b2186f98b3383c5ea6b3375dbbeadc5b4ea4
shin-carpediem <buru.aoshin@gmail.com>
2021-02-14 10:26:07 +0900

    ndexの画面で各ゲームが時間差で出てくるようにする

---
+++ static/js/games/showUp.js
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/ja/docs/Web/API/Document/getElementsByClassName
+// https://www.javadrive.jp/javascript/webpage/index4.html
+"use strict";
+
+{
+  const eachGameContainer = document.getElementsByClassName(
+    "each-game-container"
+  )[0];
+  window.setTimeout(function () {
+    eachGameContainer.classList.add("show-each-game");
+  }, 1000);
+}
