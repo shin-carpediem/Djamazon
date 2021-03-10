5203efc0183b392f76e79e8316c80dc6509e7ffa
shin-carpediem <buru.aoshin@gmail.com>
2021-03-11 00:23:33 +0900

    fix signup

--- static/js/app/signup.js
+++ static/js/app/signup.js
@@ -1,32 +1,30 @@
 "use strict";
 
 const recommendDemo = () => {
-  let signupForm = document.getElementsByClassName("signupForm")[0];
-  console.log(signupForm);
+  try {
+    let signupForm = document.getElementsByClassName("signupForm")[0];
 
-  function signUpMotion() {
-    signupForm.addEventListener("mousemove", (e) => {
-      const useDemo = confirm(
-        "You can also use demo account*. Are you want to login with it? (*The password cannot be changed.)"
-      );
-      if (useDemo) {
-        window.location.href = "/login/";
-        console.log("hoge");
-      }
-    });
-
-    if ((window.location.href = "/login/")) {
-      const username = document.getElementById("username");
-      console.log(username);
-      const password = document.getElementById("password");
-      console.log(username.value);
-      username.value = "demo@demo.com";
-      password.value = "demoaccount";
+    function signUpMotion() {
+      signupForm.addEventListener("mousemove", (e) => {
+        const useDemo = confirm(
+          "You can also use demo account*. Are you want to login with it? (*The password cannot be changed.)"
+        );
+        if (useDemo) {
+          window.location.href = "/login/";
+        }
+      });
     }
-  }
-  signUpMotion();
+    signUpMotion();
 
-  signupForm = document.getElementsByClassName("signupForm")[1];
-  signUpMotion();
+    signupForm = document.getElementsByClassName("signupForm")[1];
+    signUpMotion();
+  } catch (e) {
+    // ページがsignup.htmlからlogin.htmlに切り替わった後の動作
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+
+    username.value = "demo@demo.com";
+    password.value = "demoaccount";
+  }
 };
 recommendDemo();
