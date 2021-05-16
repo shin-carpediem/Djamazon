1cff1f7dff6676292da40faa7aa73fd57f1db1ee
shin-carpediem <buru.aoshin@gmail.com>
2021-05-16 09:29:04 +0900

    comment out google login

--- games/views.py
+++ games/views.py
@@ -144,8 +144,3 @@ def control_pingpong_point(request):
             point_history=user.point, user=user)
         userpointhistory.save()
     return redirect('games:pingpong')
-
-
-@login_required
-def dungeon(request):
-    return render(request, 'games/dungeon.html')
